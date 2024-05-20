9ce64666d46ab3663482018597c99d1f3fced531
Juancams <jc.manzanares.serrano@gmail.com>
2024-05-20 13:04:15 +0200

    Changed launcher to spawn two robots
    Signed-off-by: Juancams <jc.manzanares.serrano@gmail.com>

--- ur_simulation_gazebo/launch/ur_sim_control.launch.py
+++ ur_simulation_gazebo/launch/ur_sim_control.launch.py
@@ -27,16 +27,16 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # Author: Denis Stogl
+#
+# Modified by: Juan Carlos Manzanares Serrano
 
 from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
     OpaqueFunction,
-    RegisterEventHandler,
 )
-from launch.conditions import IfCondition, UnlessCondition
-from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
@@ -46,31 +46,25 @@ from launch_ros.substitutions import FindPackageShare
 def launch_setup(context, *args, **kwargs):
 
     # Initialize Arguments
-    ur_type = LaunchConfiguration("ur_type")
     safety_limits = LaunchConfiguration("safety_limits")
     safety_pos_margin = LaunchConfiguration("safety_pos_margin")
     safety_k_position = LaunchConfiguration("safety_k_position")
     # General arguments
     runtime_config_package = LaunchConfiguration("runtime_config_package")
-    controllers_file = LaunchConfiguration("controllers_file")
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
-    tf_prefix = LaunchConfiguration("tf_prefix")
-    start_joint_controller = LaunchConfiguration("start_joint_controller")
-    initial_joint_controller = LaunchConfiguration("initial_joint_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
     gazebo_gui = LaunchConfiguration("gazebo_gui")
-    x = LaunchConfiguration("x")
-    y = LaunchConfiguration("y")
-    z = LaunchConfiguration("z")
-
-    initial_joint_controllers = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "config", controllers_file]
-    )
 
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
     )
+
+    initial_joint_controllers = PathJoinSubstitution(
+            [FindPackageShare(runtime_config_package), "config",  "ur_controllers.yaml"]
+        )
+
+    nodes_to_start = []
 
     robot_description_content = Command(
         [
@@ -92,12 +86,6 @@ def launch_setup(context, *args, **kwargs):
             "name:=",
             "ur",
             " ",
-            "ur_type:=",
-            ur_type,
-            " ",
-            "tf_prefix:=",
-            tf_prefix,
-            " ",
             "sim_gazebo:=true",
             " ",
             "simulation_controllers:=",
@@ -113,6 +101,53 @@ def launch_setup(context, *args, **kwargs):
         parameters=[{"use_sim_time": True}, robot_description],
     )
 
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    ur5_joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur5_joint_state_broadcaster"],
+    )
+
+    ur10_joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur10_joint_state_broadcaster"],
+    )
+
+    ur5_joint_trajectory_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur5_joint_trajectory_controller"],
+    )
+
+    ur10_joint_trajectory_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur10_joint_trajectory_controller"],
+    )
+    
+    # Spawn robot
+    gazebo_spawn_robot = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-entity", "ur", "-topic", "/robot_description"],
+        output="screen",
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("gazebo_ros"), "/launch", "/gazebo.launch.py"]
+        ),
+        launch_arguments={
+            "gui": gazebo_gui
+        }.items(),
+    )
+
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
@@ -122,63 +157,15 @@ def launch_setup(context, *args, **kwargs):
         condition=IfCondition(launch_rviz),
     )
 
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["arm_1_joint_state_broadcaster", "--controller-manager", "/controller_manager"],
-    )
-
-    # Delay rviz start after `joint_state_broadcaster`
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
-
-    # There may be other controllers of the joints, but this is the initially-started one
-    initial_joint_controller_spawner_started = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[initial_joint_controller, "-c", "/controller_manager"],
-        condition=IfCondition(start_joint_controller),
-    )
-    initial_joint_controller_spawner_stopped = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[initial_joint_controller, "-c", "/controller_manager", "--stopped"],
-        condition=UnlessCondition(start_joint_controller),
-    )
-
-    # Gazebo nodes
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [FindPackageShare("gazebo_ros"), "/launch", "/gazebo.launch.py"]
-        ),
-        launch_arguments={
-            "gui": gazebo_gui,
-        }.items(),
-    )
-
-    # Spawn robot
-    gazebo_spawn_robot = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        name="spawn_ur",
-        arguments=["-entity", "ur1", "-topic", "robot_description",
-                   '-x', x, '-y', y, '-z', z],
-        output="screen",
-    )
-
-    nodes_to_start = [
-        robot_state_publisher_node,
-        joint_state_broadcaster_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner,
-        initial_joint_controller_spawner_stopped,
-        initial_joint_controller_spawner_started,
-        # gazebo,
-        gazebo_spawn_robot,
-    ]
+    nodes_to_start.append(joint_state_broadcaster_spawner)
+    nodes_to_start.append(robot_state_publisher_node)
+    nodes_to_start.append(ur5_joint_state_broadcaster_spawner)
+    nodes_to_start.append(ur10_joint_state_broadcaster_spawner)
+    nodes_to_start.append(ur5_joint_trajectory_spawner)
+    nodes_to_start.append(ur10_joint_trajectory_spawner)
+    nodes_to_start.append(gazebo_spawn_robot)
+    nodes_to_start.append(gazebo)
+    nodes_to_start.append(rviz_node)
 
     return nodes_to_start
 
@@ -186,14 +173,6 @@ def launch_setup(context, *args, **kwargs):
 def generate_launch_description():
     declared_arguments = []
     # UR specific arguments
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "ur_type",
-            description="Type/series of used UR robot.",
-            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
-            default_value="ur10e",
-        )
-    )
     declared_arguments.append(
         DeclareLaunchArgument(
             "safety_limits",
@@ -226,13 +205,6 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "controllers_file",
-            default_value="ur_controllers.yaml",
-            description="YAML file with the controllers configuration.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "description_package",
             default_value="ur_description",
             description="Description package with robot URDF/XACRO files. Usually the argument \
@@ -248,25 +220,9 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "tf_prefix",
-            default_value='""',
-            description="tf_prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
-        have to be updated.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "start_joint_controller",
             default_value="true",
             description="Enable headless mode for robot control",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "initial_joint_controller",
-            default_value="arm_1_joint_trajectory_controller",
-            description="Robot controller to start.",
         )
     )
     declared_arguments.append(
@@ -276,15 +232,6 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "gazebo_gui", default_value="true", description="Start gazebo with GUI?"
         )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument("x", default_value="0", description="X position of the robot")
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument("y", default_value="0", description="Y position of the robot")
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument("z", default_value="0", description="Z position of the robot")
     )
 
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
